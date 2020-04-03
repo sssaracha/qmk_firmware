b59d29b41214cf895bcce31e2f94d308e19982cf
davidju <davidju@zillowgroup.com>
2020-04-03 13:18:41 -0700

    update keymap

--- keyboards/kyria/keymaps/davidjudilla/config.h
+++ keyboards/kyria/keymaps/davidjudilla/config.h
@@ -37,10 +37,13 @@
 
 #define MOUSEKEY_DELAY 0
 
-#define MOUSEKEY_TIME_TO_MAX 6 
+#define MOUSEKEY_TIME_TO_MAX 12
 
 #define MOUSEKEY_MAX_SPEED 7
 
 #define MOUSEKEY_WHEEL_DELAY 0
+
+// Mod Tap
+#define PERMISSIVE_HOLD
 
    
